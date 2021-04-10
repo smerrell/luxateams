51b6795f06bbac01200be9083e008b38aa10ee92
Sam Merrell <merrell.sam@gmail.com>
2021-04-10 14:13:36 -0600

    Add code from MS example
    Got it working with some config to the app itself. I'll try and document
    that here in another commit.
    
    https://github.com/Azure-Samples/ms-identity-python-devicecodeflow

--- luxateams.py
+++ luxateams.py
@@ -1,10 +1,44 @@
 #!/usr/bin/env python3
 
 import json
+import sys
+from typing import Any
+
+import msal
+import requests
 
 
 def foo(config: Any):
-    pass
+    app = msal.PublicClientApplication(
+        config['application']['id'], None, authority=config['aad']['authority'])
+
+    result = None
+    accounts = app.get_accounts()
+    if accounts:
+        print('wow, we have an account!')
+
+    if not result:
+        print('Getting token from AAD')
+        flow = app.initiate_device_flow(scopes=config['graph']['scopes'])
+        if 'user_code' not in flow:
+            raise ValueError(
+                f"Fail to create device flow. Err; {json.dumps(flow, indent=2)}")
+
+        print(flow['message'])
+        sys.stdout.flush()
+
+        # Blocks by default
+        result = app.acquire_token_by_device_flow(flow)
+        print(json.dumps(result, indent=2))
+
+    if 'access_token' in result:
+        print("WE HAVE AN ACCESS TOKEN")
+        print(result['access_token'])
+        graph_data = requests.get('https://graph.microsoft.com/beta/me/presence',
+                                  headers={'Authorization': 'Bearer ' + result['access_token']},).json()
+        print(json.dumps(graph_data, indent=2))
+    else:
+        print(json.dumps(result, indent=2))
 
 
 def main() -> None:
