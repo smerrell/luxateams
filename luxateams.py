37cb9d4aed9464a43de9ea21e05bc713cc97b5fd
Sam Merrell <merrell.sam@gmail.com>
2021-04-12 09:58:10 -0600

    Move activity to color mapping to config file
    Represent colors in hex and turn them into Tuples when setting the
    status light. I might want to check the performance on this since it is
    running every few seconds, but for now it works.

--- luxateams.py
+++ luxateams.py
@@ -4,6 +4,7 @@ import json
 import signal
 import sys
 import threading
+from typing import Dict, List
 
 from busylight.lights.luxafor import Flag
 
@@ -12,15 +13,17 @@ from luxateams import config
 from teams.presence import Activity, get_presence
 
 
-def set_light(flag: Flag, status):
-    if status == Activity.Available:
-        flag.on([0, 255, 0])
-    if status == Activity.Busy:
-        flag.on([255, 0, 0])
-    if status == Activity.Away:
-        flag.on([255, 128, 0])
-    if status == Activity.OffWork:
-        flag.off()
+def set_light(flag: Flag, status_map: Dict[str, str], status: Activity):
+    status_color = config.to_rgb(status_map[status.value])
+    flag.on(status_color)
+    # if status == Activity.Available:
+    #     flag.on([0, 255, 0])
+    # if status == Activity.Busy:
+    #     flag.on([255, 0, 0])
+    # if status == Activity.Away:
+    #     flag.on([255, 255, 0])
+    # if status == Activity.OffWork:
+    #     flag.off()
 
 
 def graceful_exit(signal, frame):
@@ -42,7 +45,7 @@ def main() -> None:
         if 'access_token' in result:
             print('getting presence.')
             presence = get_presence(result['access_token'])
-            set_light(flag, presence)
+            set_light(flag, configuration.activity_map, presence)
 
 
 if __name__ == '__main__':
--- luxateams/config.py
+++ luxateams/config.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List
+from typing import Dict, List, Tuple
 
 import jsons
 
@@ -20,12 +20,18 @@ class GraphPermission:
     scopes: List[str]
 
 
+def to_rgb(hex: str) -> Tuple[int, int, int]:
+    color_hex = hex.lstrip('#')
+    return tuple(int(color_hex[i:i+2], 16) for i in (0, 2, 4))
+
+
 @dataclass
 class Config:
     check_interval: int
     aad: AzureAd
     application: AzureApplication
     graph: GraphPermission
+    activity_map: Dict[str, str]
 
 
 def load_config(config_file: str = 'config.json') -> Config:
